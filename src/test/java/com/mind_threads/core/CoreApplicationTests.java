7a17204f686bcc9be49bb15bf7eac09f24813fee
Jasper Oh <ohyj0906@gmail.com>
2025-01-18 14:19:08 -0800

    [Init] Initialize the project

---
+++ src/test/java/com/mind_threads/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mind_threads.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
